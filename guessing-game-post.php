19502aceec0bbb8761d9ed58bd5bbce48a1730f4
romainbruzzese <46422507+romainbruzzese@users.noreply.github.com>
2019-01-07 11:32:16 +0100

    Add files via upload

---
+++ guessing-game-post.php
@@ -0,0 +1,42 @@
+<?php
+  $oldguess = '';
+  $message = false;
+  if ( isset($_POST['guess']) ) {
+  // trick for integer / numeric parameters
+  $oldguess = $_POST['guess'] +0;
+  if ( $oldguess == 42 ) {
+  $message = "Great Job";
+  } else if ( $oldguess < 42 ) {
+  $message = "Too low"; 
+  } else {
+  $message = "Too high";
+  }
+ }
+?>
+
+<?php  $oldguess = isset($_POST['guess']) ? $_POST['guess'] : ''; ?>
+
+<html>
+<head>
+<title> Romain Bruzzese 48016789</title>
+</head>
+<body>
+<h1>Welcome to my guessing game</h1>
+<p>Guessing game...</p>
+<?php 
+if ( $message !== false ) {
+echo ("<p>$message</p>\n");
+}
+?>
+<form method="post">
+<p><label for="guess">Input Guess</label>
+<input type="text" name="guess" size="40" id="guess" value="<?= htmlentities($oldguess) ?>"/></p>
+<input type="submit"/>
+</form>
+
+<br>
+<br>
+<h2>$_POST:</h2> <?php print_r($_POST); ?>
+</body>
+</html>
+  
